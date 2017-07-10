76fd12b57c0f025f7cdf01478bb39874fadd481e
jonesbra <brandon.jones@wwt.com>
2017-07-09 23:27:20 -0500

    CREATE

---
+++ server.js
@@ -0,0 +1,19 @@
+// Core modules
+var path = require('path')
+
+// 3rd Party Modules
+var express = require('express')
+
+
+// Main Variables
+var app = express()
+// App will listen on this port
+var LISTEN_PORT = '8080'
+
+// Serve the static files
+app.use(express.static('src'))
+
+
+// Serve the app on the LISTEN_PORT
+app.listen(LISTEN_PORT)
+console.log('App listening on port: ' + LISTEN_PORT)
---
+++ src/angular/appControllers.js
@@ -0,0 +1,4 @@
+app.controller('mainCtrl', function($scope) {
+  $scope.name = 'Brandon Jones'
+  console.log('in the main ctrl')
+})
