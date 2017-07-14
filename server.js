e14b5d148e83872acd2074286aa10fef0c9cc521
jonesbra <brandon.jones@wwt.com>
2017-07-13 19:56:40 -0500

    Implemented Transcript GPA/Hours Calculation

--- server.js
+++ server.js
@@ -4,20 +4,41 @@ var path = require('path')
 // 3rd Party Modules
 var express = require('express')
 
-
 // Main Variables
-var app = express()
-// App will listen on this port
-var LISTEN_PORT = '8080'
+var frontApp = express()
+
+// frontApp will listen on this port
+var FRONTEND_PORT = '8080'
 
 // Serve the static files
-app.use(express.static('src'))
+frontApp.use(express.static('src'))
 
 // redirect if nothing else sent a response
-app.use(function redirectUnmatched(req, res) {
+frontApp.use(function redirectUnmatched(req, res) {
   res.redirect("http://www.bjones.me/")
 })
 
-// Serve the app on the LISTEN_PORT
-app.listen(LISTEN_PORT)
-console.log('App listening on port: ' + LISTEN_PORT)
+// Serve the frontApp on the LISTEN_PORT
+frontApp.listen(FRONTEND_PORT)
+console.log('Front End listening on port: ' + FRONTEND_PORT)
+
+
+var backApp = express()
+var BACKEND_PORT = '3001'
+
+backApp.get('/api/lastCommit', function(req, res) {
+  console.log('getting last commit')
+  res.status(200)
+
+  var git = require('git-last-commit')
+  git.getLastCommit(function(err, commit) {
+    // read commit object properties
+    res.write(new Date(commit.committedOn * 1000).toISOString().slice(0, 10))
+    res.end()
+  })
+  res.end('something')
+})
+
+// Serve the backApp on the LISTEN_PORT
+backApp.listen(BACKEND_PORT)
+console.log('Back End listening on port: ' + BACKEND_PORT)
